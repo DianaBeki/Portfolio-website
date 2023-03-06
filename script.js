4e51a8ae6cf174d01db899149d2f2237c427997e
Diana Beki <dianabeki54@gmail.com>
2023-03-06 23:33:22 +0300

    Call a function

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function closeIt() {
-    document.querySelector('.mobile-nav-menu').style.display = 'none';
-    console.log('wire');
-    document.body.style.overflow = 'scroll';
+function closeMenu() {
+  document.querySelector('.mobile-nav-menu').style.display = 'none';
+  document.body.style.overflow = 'scroll';
 }
 
-let union = document.querySelector('#union');
-union.addEventListener('click', function() {
-    document.querySelector('.mobile-nav-menu').style.display = 'block';
-    document.body.style.overflow = 'hidden';
-})
+closeMenu();
 
+const union = document.querySelector('#union');
+union.addEventListener('click', () => {
+  document.querySelector('.mobile-nav-menu').style.display = 'block';
+  document.body.style.overflow = 'hidden';
+});
