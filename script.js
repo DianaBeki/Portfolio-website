27128a682aa81f24b061d5d0457a3d1a7f200142
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-03-06 19:12:01 +0000

    Fix section links in mobile menu

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+function closeIt() {
+    document.querySelector('.mobile-nav-menu').style.display = 'none';
+    console.log('wire');
+    document.body.style.overflow = 'scroll';
+}
+
 let union = document.querySelector('#union');
 union.addEventListener('click', function() {
-document.querySelector('.mobile-nav-menu').style.transform = 'translateX(0)';
-});
+    document.querySelector('.mobile-nav-menu').style.display = 'block';
+    document.body.style.overflow = 'hidden';
+})
+
