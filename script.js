5c747e52bbaa46bc248d97203a7b20da2b797df4
Diana Beki <dianabeki54@gmail.com>
2023-03-09 17:20:08 +0300

    Use of object

--- script.js
+++ script.js
@@ -205,3 +205,21 @@ form.addEventListener('submit', (e) => {
     showError(true);
   }
 });
+
+// local storage
+const formData = JSON.parse(localStorage.getItem('formData')) || {
+  name: '',
+  email: '',
+  message: ''
+};
+
+const save = (key, value) => {
+  formData[key] = value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+};
+
+const nameField = form.elements['name'];
+nameField.value = formData.name;
+nameField.addEventListener('keyup', (e) => {
+  save('name', e.target.value);
+})
